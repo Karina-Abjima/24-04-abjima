8d7a7fc2cdd72dd8610ffda42d538cc69ca1c22e
Kavita <kavita.jangir@abjima.com>
2023-04-24 20:05:51 +0530

    ASP.NET

---
+++ ASPCoreWebApp/CitiesDataStore.cs
@@ -0,0 +1,42 @@
+﻿
+using ASPCoreWebApp.Models;
+using CityInfo.API.Models;
+
+namespace Cityinfo.API
+{
+    public class CitiesDataStore
+    {
+        public List<CityDto> cities { get; set; }
+
+        public CitiesDataStore()
+        {
+            cities = new List<CityDTO>()
+            {
+                new CityDto()
+            {
+                Id =1,
+                Name= "New York",
+                Description="The one with the big park"
+
+            },
+            new CityDto()
+            {
+                 Id = 2,
+                 Name ="AntWrep",
+                 Description = "The one with the big Tower"
+
+            },
+            new CityDto()
+            {
+                 Id = 3,
+                 Name = "Paris",
+                 Description = "The one with the big Theater"
+
+            },
+
+            
+            
+
+        }
+    }
+}
---
+++ ASPCoreWebApp/Models/CityDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace ASPCoreWebApp.Models
+{
+    public class CityDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? Discription { get; set; }
+
+        //public int NumberOfPointsOfInterest { get; set; }
+    }
+}
